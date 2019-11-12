d548576ff0589f0e53376fd8b43151237cedccc1
Karine Liuti <kah.liuti@gmail.com>
2019-11-12 17:31:49 -0200

    Funcionooou

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -2,18 +2,16 @@ package br.com.alura.forum.controller;
 
 import br.com.alura.forum.modelo.Curso;
 import br.com.alura.forum.modelo.Topico;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 import java.util.List;
 
 
-@Controller
+@RestController
 public class TopicosController {
 
-    @ResponseBody
     @RequestMapping("/topicos")
     public List<TopicosDto> lista() {
         Topico topicos = new Topico("Dúvida","Dúvida com spring DTO", new Curso("Spring Boot", "Programação"));
