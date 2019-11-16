9a117aa379cfd7c90d867ee9d3f58961f37155ab
Karine Liuti <kah.liuti@gmail.com>
2019-11-16 17:51:31 -0300

    Changed method to best response code: 201. It's best response when we register data on database.

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -5,12 +5,15 @@ import br.com.alura.forum.repository.CursoRepository;
 import br.com.alura.forum.repository.TopicoRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 
@@ -37,8 +40,13 @@ public class TopicosController {
 	}
     
     @PostMapping
-    public void cadastrar(@RequestBody TopicoForm form) {
+    public ResponseEntity<TopicosDto> cadastrar(@RequestBody TopicoForm form, UriComponentsBuilder uriBuilder) {
     	Topico topico = form.converter(cursoRepository);
     	topicoRepository.save(topico);
+    	
+    	URI uri = uriBuilder.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri();
+    	
+    	return ResponseEntity.created(uri).body(new TopicosDto(topico));
+    	
     }
 }
