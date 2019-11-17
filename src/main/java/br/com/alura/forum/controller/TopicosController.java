1a52f18edf69b05e7d3dbd6497ba8cad25e6c35d
Karine Liuti <kah.liuti@gmail.com>
2019-11-17 13:52:50 -0300

    Created delete method to topics

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -10,6 +10,7 @@ import br.com.alura.forum.repository.TopicoRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -49,6 +50,7 @@ public class TopicosController {
 	}
     
     @PostMapping
+    @Transactional
     public ResponseEntity<TopicosDto> cadastrar(@RequestBody @Valid TopicoForm form, UriComponentsBuilder uriBuilder) {
     	Topico topico = form.converter(cursoRepository);
     	topicoRepository.save(topico);
@@ -72,5 +74,13 @@ public class TopicosController {
     	
     	Topico topico = form.atualizar(id, topicoRepository);
     	return ResponseEntity.ok(new TopicosDto(topico));
+    }
+    
+    @DeleteMapping("/{id}")
+    @Transactional
+    public ResponseEntity<?> remover(@PathVariable Long id){
+    	topicoRepository.deleteById(id);
+    	
+    	return ResponseEntity.ok().build();
     }
 }
